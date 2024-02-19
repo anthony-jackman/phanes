ad69ed10bdfaa15baa40e4861768abfbfef674be
Anthony Jackman <energeticpixels@gmail.com>
2024-02-19 15:55:08 -0600

    FEAT: Add login screen (html/css/js) to work with pinia (#55)
    * FEAT: Add login screen (html/css/js) to work with pinia
    
    Resolves #41
    
    * Delete pnpm-lock.yaml (#56)
    
    This will be regenerated in next development
    
    ---------
    
    Co-authored-by: Anthony Jackman <57164950+anthony-jackman@users.noreply.github.com>

---
+++ src/views/User/account/Layout.vue
@@ -0,0 +1,34 @@
+<script setup>
+/*
+The account layout component is the root component of the account section / feature, it contains the outer HTML for all /account pages and a <router-view /> for rendering the currently routed component.
+*/
+import { useAuthStore } from '@/stores';
+import { router } from '@/router';
+
+// redirect home if already logged in
+const authStore = useAuthStore();
+if (authStore.user) {
+    router.push('/');
+}
+</script>
+
+<template>
+  <div class="wrapper">
+
+      <!-- <div class="wrapper"> -->
+        <router-view />
+      <!-- </div> -->
+
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: rgba(0, 0, 0, 0.2);
+}
+</style>
